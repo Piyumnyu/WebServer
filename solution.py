6f12f939489f35470646afc3e52cf269636204b2
Piyumnyu <pkm5782@nyu.edu>
2022-02-18 18:39:33 +0530

    Attempt 3

--- solution.py
+++ solution.py
@@ -15,36 +15,34 @@ def webServer(port=13331):
   while True:
     #Establish the connection
     #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = f.read()
         
         #Send one HTTP header line into socket.
-        #Fill in start
-
-        #Fill in end
+        connectionSocket.send(outputdata)
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
           connectionSocket.send(outputdata[i].encode())
-
-        connectionSocket.send("\r\n".encode())
+        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
         connectionSocket.close()
+
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
         #Fill in end
 
 
         #Close client socket
         #Fill in start
-
+        connectionSocket.close()
         #Fill in end
 
     except (ConnectionResetError, BrokenPipeError):
